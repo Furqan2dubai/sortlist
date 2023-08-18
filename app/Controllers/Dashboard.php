dc55bf382efdb0460203597199ae118aa659fce0
Furqan Hussain <45510981+Furqan2dubai@users.noreply.github.com>
2023-08-18 14:48:37 +0800

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 class Dashboard extends BaseController
 {
     public function index()
-    { 
+    { die;
         $data["title"] = "dashboard"; 
         return view("admin/dashboard",$data);
     }
